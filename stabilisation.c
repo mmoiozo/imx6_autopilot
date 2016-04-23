3809d384175fd535d1d53f2371cbdbf0a320ed2f
mmoiozo <ozo1994@live.nl>
2016-04-23 20:26:58 +0200

    cascaded pid working fix d and i

--- stabilisation.c
+++ stabilisation.c
@@ -95,46 +95,48 @@ void PID_cascaded(double delta_t)
     float command_angle_pitch = (((float)y_com)/100)+90+18.9;//((float)y_com/100)+90;+(1891/100)
     float command_angle_roll = (((float)x_com)/100)+90;//(-(float)x_com/100)+90;
     
-    float command_rate_pitch = (((float)y_com)/2);//no scaling 30 deg/sec is 30*130=3900 lsb
-    float command_rate_roll = (((float)x_com)/2);//
+    float command_rate_pitch = (((float)y_com)/1);//no scaling 30 deg/sec is 30*130=3900 lsb
+    float command_rate_roll = (((float)x_com)/1);//
     
     //ATTITUDE LOOP
     
     float error_pitch = comp_angle_pitch - command_angle_pitch;
     float error_roll = comp_angle_roll - command_angle_roll;
     
-    float p_cmd_pitch = error_pitch*((float)gain_P_X/15);
-    float p_cmd_roll = error_roll*((float)gain_P_Y/15);
+    float p_cmd_pitch = error_pitch*((float)gain_P_Y/1);//was gain_P_X
+    float p_cmd_roll = error_roll*((float)gain_P_Y/1);
     
     i_cmd_pitch += error_pitch*((float)gain_i_X/20)*delta_t;
-    if(i_cmd_pitch > 120)i_cmd_pitch = 120;//prevent integral windup
-    if(i_cmd_pitch < -120)i_cmd_pitch = -120;
+    if(i_cmd_pitch > 1200)i_cmd_pitch = 1200;//prevent integral windup
+    if(i_cmd_pitch < -1200)i_cmd_pitch = -1200;
     i_cmd_roll += error_roll*((float)gain_i_Y/20)*delta_t;
-    if(i_cmd_roll > 120)i_cmd_roll = 120;//prevent integral windup
-    if(i_cmd_roll < -120)i_cmd_roll = -120;
+    if(i_cmd_roll > 1200)i_cmd_roll = 1200;//prevent integral windup
+    if(i_cmd_roll < -1200)i_cmd_roll = -1200;
     
     float pitch_control_rate = p_cmd_pitch + i_cmd_pitch;
     float roll_control_rate = p_cmd_roll + i_cmd_roll;
     
     //RATE LOOP
     
-    float rate_error_pitch = (float)(-y_gyro_raw) - command_rate_pitch;
-    float rate_error_roll = (float)x_gyro_raw - command_rate_roll;
+    //Rate control
+    //float rate_error_pitch = (float)(-y_gyro_raw) - command_rate_pitch;
+    //float rate_error_roll = (float)x_gyro_raw - command_rate_roll;
+    
+    //Outer loop control
+    float rate_error_pitch = (float)(-y_gyro_raw) - (-pitch_control_rate) ;
+    float rate_error_roll  = (float)x_gyro_raw - (-roll_control_rate);
     
     float p_cmd_pitch_r = rate_error_pitch*((float)gain_P_X/2000);
-    float p_cmd_roll_r = rate_error_roll*((float)gain_P_Y/2000);
+    float p_cmd_roll_r = rate_error_roll*((float)0/2000);//was gain_P_Y but is now temporary used by outer loop P 
     
-    float d_cmd_pitch = ((rate_error_pitch-prev_error_pitch)/delta_t)*((float)gain_D_X/200);
-    float d_cmd_roll = ((rate_error_roll-prev_error_roll)/delta_t)*((float)gain_D_Y/200);
+    float d_cmd_pitch = ((rate_error_pitch-prev_error_pitch)/delta_t)*((float)gain_D_X/20000);
+    float d_cmd_roll = ((rate_error_roll-prev_error_roll)/delta_t)*((float)gain_D_Y/20000);
     
     prev_error_pitch = rate_error_pitch;
     prev_error_roll = rate_error_roll;
     
     pitch_control = p_cmd_pitch_r + d_cmd_pitch;
     roll_control  = p_cmd_roll_r  + d_cmd_roll;
-    
-    //int throttle = 205 + (t_com + 3276)/26;
-    //if(throttle < 205)throttle = 205;
     
     int throttle = 819 + (t_com + 3276)/7;
     if(throttle < 819)throttle = 819;
